7773b332e14939e6520efa1d37ed71b53e3e5dcc
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-05-06 17:26:35 -0300

    dia 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Center, Container, Flex, FormControl, Heading, HStack, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Text, VStack } from "@chakra-ui/react"
+import { AspectRatio, Box, Button, Center, Container, Flex, FormControl, Heading, HStack, Image, Input, NumberDecrementStepper, NumberIncrementStepper, NumberInput, NumberInputField, NumberInputStepper, Text, VStack } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 import './index.css'
 
@@ -6,40 +6,49 @@ import './index.css'
 interface Regalo {
     name: string,
     id: number,
-    cantidad: number
+    cantidad: number,
+    image: string
+}
+interface GiftForm {
+    name: string,
+    image: string
 }
 
 export const App = () => {
-    
-    const init = () =>{
-        const strings = localStorage.getItem('regalos') || '[]'
-        const stringsParsed = JSON.parse(strings)
-        console.log(stringsParsed)
-        return stringsParsed
-    }
 
-    const [regalos, setRegalos] = useState<Regalo[]>( ()=> JSON.parse( localStorage.getItem('regalos') || '[]')    )
+    const [regalos, setRegalos] = useState<Regalo[]>(() => JSON.parse(localStorage.getItem('regalos') || '[]'))
     const [cantidad, setCantidad] = useState<number>(1)
-    const [formValue, setFormValue] = useState<string>('')
-    
+    const [formValue, setFormValue] = useState<GiftForm>({
+        name: '',
+        image: ''
+    })
+
     useEffect(() => {
         const regalosString = JSON.stringify(regalos)
         localStorage.setItem('regalos', regalosString)
     }, [regalos])
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setFormValue(e.target.value)
+        setFormValue(prev => (
+            {
+                ...prev,
+                [e.target.name]: e.target.value
+            })
+        )
     }
     const handleNumberChange = (e: number): void => {
         setCantidad(e)
     }
     const handleAdd = (): void => {
-        if (!formValue) return;
-        const res = regalos.filter(regalo => regalo.name === formValue)
+        if (!formValue.name) return;
+        const res = regalos.filter(regalo => regalo.name === formValue.name)
         if (res.length) return;
         const id = new Date().getTime()
-        setRegalos(prev => [...prev, { name: formValue, id, cantidad }]);
-        setFormValue('')
+        setRegalos(prev => [...prev, { ...formValue, id, cantidad }]);
+        setFormValue({
+            name: '',
+            image: ''
+        })
     }
 
     const handleDelete = (id: number): void => {
@@ -52,18 +61,23 @@ export const App = () => {
     return (
         <Container bg='gray.300' w='full' h='100vh' minWidth='container.lg'
         >
-
             <VStack>
                 <Heading>
                     Regalos
                 </Heading>
                 {
-                    regalos.map(regalo => (
-                        <Flex key={regalo.id} justifyContent='center' alignItems='center'>
+                    regalos.map( ({id, name, cantidad, image}) => (
+                        <Flex key={id} justifyContent='flex-end' w='md' alignItems='center'>
+                            {
+                             image &&
+                             <AspectRatio ratio={1} minW='100px' m={4} >
+                                 <Image src={image} />
+                             </AspectRatio>   
+                            }
                             <Text>
-                                {regalo.name} {regalo.cantidad > 1 && `x ${regalo.cantidad}`}
+                                {name} {cantidad > 1 && `x ${cantidad}`}
                             </Text>
-                            <Button mx={4} bgColor='red.200' onClick={() => handleDelete(regalo.id)}>Eliminar</Button>
+                            <Button mx={4} bgColor='red.200' onClick={() => handleDelete(id)}>Eliminar</Button>
                         </Flex>
                     )
                     )
@@ -76,7 +90,10 @@ export const App = () => {
             </VStack>
             <HStack w='sm'>
                 <FormControl>
-                    <Input name="input" placeholder="Nuevo regalo" variant='filled' value={formValue} onChange={(e) => handleChange(e)} />
+                    <Input name="name" placeholder="Nuevo regalo" variant='filled' value={formValue.name} onChange={(e) => handleChange(e)} />
+                </FormControl>
+                <FormControl>
+                    <Input name="image" placeholder="Link de la imagen" value={formValue.image} variant='filled' onChange={(e) => handleChange(e)} />
                 </FormControl>
                 <NumberInput name="numberinput"
                     value={cantidad}
