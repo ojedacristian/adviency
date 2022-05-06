1de70d4b559936487e0ecb7a75d2241a9c29934f
Cristian Ojeda <cristianojeda.maimo@gmail.com>
2022-05-06 12:32:08 -0300

    dia 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,49 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { Box, Button, Container, Flex, FormControl, Heading, HStack, Input, Text, VStack } from "@chakra-ui/react"
+import { useState } from "react"
+import './index.css'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+export const App = () => {
+    const [regalos, setRegalos] = useState([{ name: 'medias', id: 1 }, {name: 'caramelos', id: 2}, { name: ' vitel tone', id:3}])
+    const [formValue, setFormValue] = useState('')
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormValue(e.target.value)
+    }
+    const handleAdd = () => {
+        const id = new Date().getTime()
+        setRegalos(prev => [...prev, { name: formValue, id }]);
+        setFormValue('')
+    }
+
+    const handleDelete = (id:number):void => {
+        setRegalos( prev => prev.filter( regalo => regalo.id !== id) )
+    }
+    return (
+        <Container bg='gray.300' w='full' h='100vh' minWidth='container.lg'
+        >
+
+            <VStack>
+                <Heading>
+                    Regalos
+                </Heading>
+                {
+                    regalos.map(regalo => (
+                        <Flex key={regalo.id} justifyContent='center' alignItems='center'>
+                            <Text>
+                                {regalo.name}
+                            </Text>
+                            <Button mx={4} bgColor='red.200' onClick={() => handleDelete(regalo.id) }>Eliminar</Button>
+                        </Flex>
+                    )
+                    )
+                }
+            </VStack>
+            <HStack w='sm'>
+                <FormControl>
+                    <Input placeholder="Nuevo regalo" variant='filled' value={formValue} onChange={(e) => handleChange(e)} />
+                </FormControl>
+                <Button onClick={handleAdd}>Agregar</Button>
+            </HStack>
+        </Container >
+    )
+}
